8a5e3241edd84f331a2ee64f1853dd450db43d37
albertgoncalves <goncalves.albert@gmail.com>
2021-03-01 22:13:27 -0500

    refine into sep module

--- src/Main.hs
+++ src/Main.hs
@@ -1,77 +1,14 @@
-import Data.Map.Strict
-  ( Map,
-    empty,
-    fromList,
-    insertWith,
-    lookupGT,
-    singleton,
-    toList,
-    unionWith,
-    (!),
-  )
-import Data.Text (Text, lines, words)
+import Control.Monad (replicateM_)
+import Data.Functor ((<&>))
 import Data.Text.IO (readFile)
-import Data.Word (Word32)
-import Random (randomBounded, seed)
+import Data.Time (diffTimeToPicoseconds, getCurrentTime, utctDayTime)
+import Markov (chainToText, distrsToChain, seed, textToDistrs)
 import System.Environment (getArgs)
-import Prelude hiding (Word, lines, readFile, words)
-
-data Token
-  = Start
-  | Word Text
-  | End
-  deriving (Eq, Ord, Show)
-
-type Table = Map Token Word32
-
-type Distr = (Word32, Map Word32 Token)
-
-textToTokens :: Text -> [[Token]]
-textToTokens = map (\x -> Start : map Word (words x) ++ [End]) . lines
-
-combineTables :: Table -> Table -> Table
-combineTables = unionWith (+)
-
-insertPair :: Token -> Token -> Map Token Table -> Map Token Table
-insertPair k v = insertWith combineTables k (singleton v 1)
-
-updateChain :: Map Token Table -> [Token] -> Map Token Table
-updateChain m xs = foldr (uncurry insertPair) m $ zip xs $ tail xs
-
-makeChain :: [[Token]] -> Map Token Table
-makeChain = foldr (unionWith combineTables . updateChain empty) empty
-
-tableToDistr :: Table -> Distr
-tableToDistr t = (n, fromList $ zip ns' ws)
-  where
-    (ws, ns) = unzip $ toList t
-    ns' = scanl1 (+) ns
-    n = sum ns
-
-chainToDistrs :: Map Token Table -> Map Token Distr
-chainToDistrs = fmap tableToDistr
-
-nextToken :: Map Token Distr -> Token -> IO Token
-nextToken m t = do
-  i <- randomBounded n
-  return $ case lookupGT i m' of
-    Just (_, t') -> t'
-    Nothing -> undefined
-  where
-    (n, m') = m ! t
-
-generate :: Map Token Distr -> IO [Token]
-generate m = (Start :) <$> f m Start
-  where
-    f _ End = return []
-    f m' t = do
-      t' <- nextToken m' t
-      (t' :) <$> f m' t'
+import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-  seed 0 0
-  filename <- head <$> getArgs
-  text <- readFile filename
-  let m = chainToDistrs $ makeChain $ textToTokens text
-  print =<< generate m
+  t <- fromInteger . diffTimeToPicoseconds . utctDayTime <$> getCurrentTime
+  seed t 0
+  distrs <- (getArgs >>= readFile . head) <&> textToDistrs
+  replicateM_ 10 (print . chainToText =<< distrsToChain distrs)
---
+++ src/Markov.hs
@@ -0,0 +1,74 @@
+module Markov (distrsToChain, textToDistrs, chainToText, seed) where
+
+import Data.Map.Strict
+  ( Map,
+    empty,
+    fromList,
+    insertWith,
+    lookupGT,
+    singleton,
+    toList,
+    unionWith,
+    (!),
+  )
+import Data.Maybe (mapMaybe)
+import Data.Text (Text, lines, unwords, words)
+import Data.Word (Word32)
+import Random (randomBounded, seed)
+import Prelude hiding (Word, lines, unwords, words)
+
+data Token
+  = Start
+  | Word Text
+  | End
+  deriving (Eq, Ord, Show)
+
+type Table = Map Token Word32
+
+type Distr = (Word32, Map Word32 Token)
+
+textToTokens :: Text -> [[Token]]
+textToTokens = map (\x -> Start : map Word (words x) ++ [End]) . lines
+
+combineTables :: Table -> Table -> Table
+combineTables = unionWith (+)
+
+insertTokens :: Token -> Token -> Map Token Table -> Map Token Table
+insertTokens a b = insertWith combineTables a (singleton b 1)
+
+updateTables :: Map Token Table -> [Token] -> Map Token Table
+updateTables m xs = foldr (uncurry insertTokens) m $ zip xs $ tail xs
+
+tableToDistr :: Table -> Distr
+tableToDistr t = (sum ns, fromList $ zip (scanl1 (+) ns) ws)
+  where
+    (ws, ns) = unzip $ toList t
+
+makeDistrs :: [[Token]] -> Map Token Distr
+makeDistrs =
+  fmap tableToDistr
+    . foldr (unionWith combineTables . updateTables empty) empty
+
+distrsToChain :: Map Token Distr -> IO [Token]
+distrsToChain m = f Start
+  where
+    f t = (t :) <$> loop t
+
+    loop :: Token -> IO [Token]
+    loop End = return []
+    loop t = do
+      i <- randomBounded n
+      case lookupGT i m' of
+        Just (_, t') -> f t'
+        Nothing -> undefined
+      where
+        (n, m') = m ! t
+
+textToDistrs :: Text -> Map Token Distr
+textToDistrs = makeDistrs . textToTokens
+
+chainToText :: [Token] -> Text
+chainToText = unwords . mapMaybe f
+  where
+    f (Word x) = Just x
+    f _ = Nothing
